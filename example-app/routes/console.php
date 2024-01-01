9594880cf9b83565b1aff70284f1b7218637460c
Philip <philiposun@gmail.com>
2024-01-01 23:51:48 +0100

    Update console.php

--- example-app/routes/console.php
+++ example-app/routes/console.php
@@ -18,6 +18,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Artisan::command('inspire', function() {
-    $this->comment(inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Artisan::command('inspire', function() {
+//     $this->comment(inspiring::quote());
+// })->purpose('Display an inspiring quote');
