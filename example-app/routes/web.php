e032f3f6e55a8d806fce62a7a5af7371977524eb
Philip <philiposun@gmail.com>
2024-01-09 02:50:47 +0100

    Update web.php

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,7 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-/
+
+Route::get('/hello', function () {
+    return response('<h1>Hello World</h1>', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
 
 Route::get('/posts/{id}', function($id) {
     return response('Post ' . $id,);
